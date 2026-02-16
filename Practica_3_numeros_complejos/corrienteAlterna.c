eedd209a2d99f579d78ad301a1eba69c797e1508
aitorClld <aitor160907@gmail.com>
2026-02-16 21:16:27 +0100

    Solucion practica 3

---
+++ Practica_3_numeros_complejos/corrienteAlterna.c
@@ -0,0 +1,48 @@
+#include "complejos.h"
+
+TComplejo ImpedanciaL(float l, float w) {
+    TComplejo zl;
+    zl.re = 0;
+    zl.im = l*w;
+    return zl;
+}
+
+
+TComplejo ImpedanciaR(float r) {
+    TComplejo zr;
+    zr.re = r;
+    zr.im = 0;
+    return zr;
+}
+
+TComplejo ImpedanciaC(float c, float w) {
+    TComplejo zc;
+    zc.re = 0;
+    zc.im = -1/(c*w);
+    return zc;
+}
+
+TComplejo ImpedanciaTotal(TComplejo zr, TComplejo zc, TComplejo zl) {
+    TComplejo zTotal;
+    zTotal = sumaComplejos(zr,zc);
+    zTotal = sumaComplejos(zTotal,zl);
+    return zTotal;
+}
+
+float FrecuenciaAngular(float f) {
+    float w;
+    w = f*2*M_PI;
+    return w;
+}
+
+TComplejo Intensidad(TComplejo z, TComplejo v) {
+    TComplejo i;
+    i = divisionComplejos(v,z);
+    return i;
+}
+
+TComplejo CaidaPotencial(TComplejo z, TComplejo i) {
+    TComplejo v;
+    v = multiplicacionComplejos(i,z);
+    return v;
+}
