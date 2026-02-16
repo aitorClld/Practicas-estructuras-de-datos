cf986b0892677701c41e4801fc943bdeaab2793c
aitorClld <aitor160907@gmail.com>
2026-02-16 21:18:31 +0100

    Limpieza: eliminando archivos duplicados y configuración de IDE

--- corrienteAlterna.h
+++
@@ -1,8 +0,0 @@
-#include "complejos.h"
-TComplejo ImpedanciaL(float l, float w);
-TComplejo ImpedanciaR(float r);
-TComplejo ImpedanciaC(float c, float w);
-TComplejo ImpedanciaTotal(TComplejo zr, TComplejo zc, TComplejo zl);
-float FrecuenciaAngular(float f);
-TComplejo Intensidad(TComplejo z, TComplejo v);
-TComplejo CaidaPotencial(TComplejo z, TComplejo i);
